eb252f227f1bac76837c6eb12c0b5c3908dcd35a
Dongdong Yang <hi@dongdong.ca>
2017-07-21 16:43:06 -0400

    Update display-value-as-column.js

--- display-value-as-column.js
+++ display-value-as-column.js
@@ -6,33 +6,38 @@
 **/
 
 jQuery.fn.extend({
-  DisplayValueAsColumn: function(items) {
-    var self=this;
-    var $self=$(this);
-    if(items){
-       Object.keys(items).forEach(function(key){
-          var option=$('<option>');
-          option.val(key);
-          option.text(items[key]);
-          $self.append(option);
-      }) 
+    BeautifySelect: function (options) {
+        var self = this;
+        var $self = $(this);
+        if (options && options.items) {
+            Object.keys(options.items).forEach(function (key) {
+                var option = $('<option>');
+                option.val(key);
+                option.text(options.items[key]);
+                $self.append(option);
+            })
+        }
+        $self.attr('size', $self.find('option').length);
+        $self.wrap('<div class="beautify-select"></div>').wrap('<span>');
+
+        var displaySelect = $('<select class="display-select"></select>');
+        $self.before(displaySelect);
+        $self.before($('<br>'));
+        $self.addClass('value-select');
+
+        if (options && options.showValue) {
+            $self.addClass('display-value-as-column');
+        }
+
+        displaySelect.on('click', function () {
+            $self.slideToggle(110);
+        })
+        $self.on('click', function () {
+            $self.hide();
+            displaySelect.empty();
+            var selectedOption = $self.find('option:selected').clone().appendTo(displaySelect);
+        })
     }
-    $self.attr('size', $self.find('option').length);
-               
-    $self.wrap('<div class="display-value-as-column"></div>').wrap('<span>');
-    
-    var displaySelect=$('<select class="display-select"></select>');
-    $self.before(displaySelect);
-    $self.before($('<br>'));
-    $self.addClass('value-select'); 
-    
-    displaySelect.on('click',function(){
-      $self.slideToggle(110);
-    })
-    $self.on('click',function(){
-      $self.hide();
-      displaySelect.empty();
-      var selectedOption=$self.find('option:selected').clone().appendTo(displaySelect);
-    })
-  }
 });
+
+
