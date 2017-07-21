13e95c465aa95af228dfa0766abb7f2e7a97409b
Dongdong Yang <hi@dongdong.ca>
2017-07-21 15:35:30 -0400

    Update display-value-as-column.js

--- display-value-as-column.js
+++ display-value-as-column.js
@@ -2,7 +2,7 @@
  * @author Dongdong Yang <hi@dongdong.ca>
  * Copyright (c) 2017 Dongdong Yang - released under MIT License
  * https://github.com/stupidong/display-value-as-column.js
- * Usage: readme.md
+ * Usage: $('#yourselect').DisplayValueAsColumn(data);
 **/
 
 jQuery.fn.extend({
@@ -15,9 +15,10 @@ jQuery.fn.extend({
           option.val(key);
           option.text(items[key]);
           $self.append(option);
-      })
-      $self.attr('size', Object.keys(items).length);
-    }  
+      }) 
+    }
+    $self.attr('size', $self.find('option').length);
+               
     $self.wrap('<div class="display-value-as-column"></div>').wrap('<span>');
     
     var displaySelect=$('<select class="display-select"></select>');
